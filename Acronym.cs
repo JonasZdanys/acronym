507faef2ee891534f6748ab150e91a73b29a7c8b
Jonas Zdanys <jonas.zdanys@teliacompany.com>
2021-04-21 16:55:02 +0300

    complete

---
+++ Acronym.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+public static class Acronym
+{
+    public static string Abbreviate(string phrase)
+    {
+        char[] separators = { ' ', '-', '_' };
+        string[] words = phrase.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+        var firstLetters = words.Select(letter => letter[0].ToString().ToUpper());
+            var a = firstLetters.Aggregate((abbr, letter) => abbr += letter);
+        return a;
+    }
+}
